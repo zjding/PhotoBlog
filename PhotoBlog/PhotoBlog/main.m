8eaa519f2af343f37e7d2239b4d83c8442821354
Jason Ding <zjding@gmail.com>
2012-10-05 15:43:12 -0500

    First check in

---
+++ PhotoBlog/PhotoBlog/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoBlog
+//
+//  Created by Zhijun Ding on 10/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "zjdAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([zjdAppDelegate class]));
+    }
+}
---
+++ PhotoBlog/PhotoBlog/zjdAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  zjdAppDelegate.h
+//  PhotoBlog
+//
+//  Created by Zhijun Ding on 10/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface zjdAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
